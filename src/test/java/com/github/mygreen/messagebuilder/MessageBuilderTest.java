4cf714831843586d45e443dae53ee41d1938460e
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2020-06-28 15:05:43 +0900

    テストケースの追加

--- src/test/java/com/github/mygreen/messagebuilder/MessageBuilderTest.java
+++ src/test/java/com/github/mygreen/messagebuilder/MessageBuilderTest.java
@@ -2,6 +2,12 @@ package com.github.mygreen.messagebuilder;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -67,4 +73,68 @@ public class MessageBuilderTest {
         assertThat(result).isEqualTo("再帰的なメッセージ：{abc}={max}");
 
     }
+
+    @Test
+    void testVarWithAnno() {
+
+        String result = messageBuilder.create("test.varWithAnno")
+                .varWithAnno("anno", Entity.class)
+                .format();
+
+        assertThat(result).isEqualTo("変数の初期フォーマット：アノテーション「@Entity」");
+
+    }
+
+    @Test
+    void testVarWithEnum() {
+
+        String result = messageBuilder.create("test.varWithEnum")
+                .varWithEnum("enum", Role.Admin)
+                .format();
+
+        assertThat(result).isEqualTo("変数の初期フォーマット：列挙型「Role#Admin」");
+
+    }
+
+    @Test
+    void testVarWithClass() {
+        String result = messageBuilder.create("test.varWithClass")
+                .varWithClass("class", MessageBuilder.class)
+                .format();
+
+        assertThat(result).isEqualTo("変数の初期フォーマット：クラス「com.github.mygreen.messagebuilder.MessageBuilder」");
+    }
+
+    @Test
+    void testVarWithClasses() {
+        String result = messageBuilder.create("test.varWithClasses")
+                .varWithClass("classes", MessageBuilder.class, SpelExpressionEvaluator.class)
+                .format();
+
+        assertThat(result).isEqualTo("変数の初期フォーマット：クラス「com.github.mygreen.messagebuilder.MessageBuilder, com.github.mygreen.messagebuilder.expression.SpelExpressionEvaluator」");
+    }
+
+    /**
+     * テスト用アノテーション
+     *
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.TYPE})
+    @Documented
+    @interface Entity {
+
+        String name() default "";
+
+    }
+
+    /**
+     * テスト用列挙型
+     *
+     */
+    enum Role {
+
+        Normal,
+        Admin
+    }
+
 }
